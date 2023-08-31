ddfe2d931b7676a947d6b16732a822277c3318e9
Shaun <shaun@thenetninja.co.uk>
2023-08-31 10:34:29 +0100

    lesson-11

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
+import { client } from "@/lib/db"
 import Link from 'next/link'
 
 const getBooks = async () => {
-  
+  const result = await client.zRangeWithScores('books', 0, -1)
+
+  // [{value: 'abc', score: '2397'}, {value: 'def', score: '4233'}]
+
+  const books = await Promise.all(result.map(b => {
+    return client.hGetAll(`books:${b.score}`)
+  }))
+
+  return books
 }
 
 export default async function Home() {
+  const books = await getBooks()
 
   return (
     <main>
@@ -13,7 +23,14 @@ export default async function Home() {
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
       
-      <p>List of books here.</p>
+      {books.map(book => (
+        <div key={book.title} className="card"> 
+          <h2>{book.title}</h2>
+          <p>By {book.author}</p>
+          <p>{book.blurb}</p>
+          <p>Rating: {book.rating}</p>
+        </div>
+      ))}
     </main>
   )
 }
