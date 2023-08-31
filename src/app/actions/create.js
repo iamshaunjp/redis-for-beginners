ae8c2951979f11e2a8c17a0ef6a3aef87bc4819c
Shaun <shaun@thenetninja.co.uk>
2023-08-31 10:23:58 +0100

    lesson-8

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,21 +1,8 @@
 'use server'
 
-import { client } from "@/lib/db"
+// import { client } from "@/lib/db"
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
-
-  // create a book id
-  const id = Math.floor(Math.random() * 100000)
   
-  // save new hash for the book
-  await client.hSet(`books:${id}`, {
-    title,
-    rating,
-    author,
-    blurb
-  })
-
-  redirect('/')
 }
