8a5831f432c31cf80adef51bb52ca1fcd1593c63
Shaun <shaun@thenetninja.co.uk>
2023-08-31 10:30:19 +0100

    lesson-10

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -8,6 +8,16 @@ export async function createBook(formData) {
 
   // create a book id
   const id = Math.floor(Math.random() * 100000)
+
+  // add the book to the sorted set
+  const unique = await client.zAdd('books', {
+    value: title,
+    score: id
+  }, { NX: true })
+
+  if (!unique) {
+    return {error: 'That book has already been added.'}
+  }
   
   // save new hash for the book
   await client.hSet(`books:${id}`, {
