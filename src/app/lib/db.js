2964634f6c54d1ef4b596ebccd96b441ec758569
Shaun <shaun@thenetninja.co.uk>
2023-08-31 09:52:42 +0100

    lesson-8

---
+++ src/app/lib/db.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis'
+
+const client = createClient({
+  password: process.env.REDIS_PW,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  }
+})
+
+client.on('error', err => console.log(err))
+
+if (!client.isOpen) {
+  await client.connect()
+}
+
+// client.set('name', 'mario')
+
+export { client }
